b177d47d44f3ba2a3329e53664595b13c839e08f
Ryan Hodge <ryanhodge240@gmail.com>
2023-02-23 15:07:36 -0500

    Removed website submodule as hosting on git is no longer necessary

---
+++ tread-bot-website/src/AppStyles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default class AppStyles {
+	static readonly AppContainer = styled.div`
+		display: flex;
+		flex-direction: column;
+		justify-items: center;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+	`
+}
