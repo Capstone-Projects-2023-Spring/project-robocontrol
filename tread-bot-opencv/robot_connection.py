362f828e925d218b6466d3afa2f0814740f6d098
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-13 18:34:11 -0400

    Update template

--- tread-bot-opencv/robot_connection.py
+++ tread-bot-opencv/robot_connection.py
@@ -12,7 +12,9 @@ WEBSOCKET = "ws://192.168.2.3:8998"
 def base64_to_cv2(img):
     img_bytes = base64.b64decode(img)
     im_arr = np.frombuffer(img_bytes, dtype=np.uint8)
+    
     return cv2.imdecode(im_arr, flags=1)
+
 
 # The main function that will handle connection and communication with the server
 async def listen():
@@ -24,7 +26,6 @@ async def listen():
         while True:
             packet = await ws.recv()
             image = base64_to_cv2(packet)
-
             cv2.imshow('Socket data', image)
             c = cv2.waitKey(1)
             if c == 27:
