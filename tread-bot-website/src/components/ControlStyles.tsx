bef517e8fd3f845e120aa62064d111322ba329c4
antKeyes <keyes.anthony@gmail.com>
2023-03-16 18:09:41 -0400

    changed buttons to squares

--- tread-bot-website/src/components/ControlStyles.tsx
+++ tread-bot-website/src/components/ControlStyles.tsx
@@ -3,7 +3,17 @@ import styled from 'styled-components'
 export default class ControlStyles {
 	static readonly ControlContainer = styled.div`
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
 		width: 100%;
+
+		button {
+			width: 100px;
+			height: 100px;
+			margin: 5px;
+		}
+
 	`
 }
