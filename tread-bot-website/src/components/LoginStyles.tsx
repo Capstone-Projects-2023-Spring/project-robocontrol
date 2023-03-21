52567b00bdc985b407e4aadd2f590b8632431ae9
antKeyes <keyes.anthony@gmail.com>
2023-03-20 21:56:12 -0400

    Update LoginContainer

--- tread-bot-website/src/components/LoginStyles.tsx
+++ tread-bot-website/src/components/LoginStyles.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
 export default class LoginStyles {
-	static readonly LoginContainer = styled.div`		
-		display: flex;
+	static readonly LoginContainer = styled.div`
+    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding: 2rem;
-    min-height: 50vh;
-	`
+    min-height: 20vh;
+    max-height: 25vh;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06);
+  `;
 
 	static readonly InputContainer = styled.div`
     //...
