d2f547333c77b9b5370593d6aa63338480473116
Kevin Jang <tul56945@temple.edu>
2023-03-29 15:01:26 -0400

    init

---
+++ tread-bot-opencv/image_processing/automate.py
@@ -0,0 +1,12 @@
+## This is a general outline only. 
+
+#Define boundary lines (The line within which the robot will keep itself in between.)
+    # Considerations for boundary lines
+    # Are the lines the same color? Logically they would be however is there any reasonable scenario within which they will not be the same?
+    # Does the robot find two lines that "match" as close as possible or are these boundary lines explicitly defined. In which case: 
+        #How do the boundary lines get defined? Are they kept within a data field or is it user defined?
+        #If it is user defined, how does the user input what is the boundary line? Is it by explicit RGB values, or a drop down list of colours, etc.
+
+
+#Detect boundary lines
+    #
