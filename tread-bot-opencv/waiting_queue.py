5e0782523b70cdccd420f0af061d157f4ccf9137
Qian Peng <tug82109@temple.edu>
2023-03-29 15:34:44 -0400

    add FIFO

--- tread-bot-opencv/waiting_queue.py
+++ tread-bot-opencv/waiting_queue.py
@@ -1,15 +1,33 @@
+import asyncio
 from collections import deque
 
-class WaitingQueue:
 
+class WaitingQueue:
     def __init__(self):
         self.queue = deque()
+        self.active_user = None
 
-    def add_user(self, user):
-        self.queue.append(user)
+    def add_user(self, websocket):
+        self.queue.append(websocket)
+        if len(self.queue) == 1:
+            self.active_user = websocket
+            asyncio.create_task(self.notify_position())
 
-    def remove_user(self, user):
-        self.queue.remove(user)
+    def remove_user(self, websocket):
+        self.queue.remove(websocket)
+        if self.active_user == websocket:
+            self.active_user = None
+            self.next_user()
 
-    def get_current_user(self):
-        return self.queue[0] if len(self.queue) > 0 else None
+    def get_active_user(self):
+        return self.active_user
+
+    def next_user(self):
+        if len(self.queue) > 0:
+            self.active_user = self.queue.popleft()
+            asyncio.create_task(self.notify_position())
+
+    async def notify_position(self):
+        for idx, websocket in enumerate(self.queue):
+            position = idx + 1
+            await websocket.send('{"type": "queue_position", "position": ' + str(position) + '}')
