f50ff7f574b728bbef9eea91225abd937c19dc33
Qian Peng <tug82109@temple.edu>
2023-04-03 13:28:42 -0400

    test file bug fixs

--- tread-bot-opencv/multi_clients_test.py
+++ tread-bot-opencv/multi_clients_test.py
@@ -11,9 +11,10 @@ async def connect_and_send_message(uri, message):
 
 async def main():
     uri = "ws://localhost:10334"
-    message = "This is a Test Message"
+    message = '{"message": "This is a Test Message"}'
 
-    tasks = [connect_and_send_message(uri, message) for _ in range(3)]
+
+    tasks = [connect_and_send_message(uri, message) for _ in range(6)]
     await asyncio.gather(*tasks)
 
 
--- tread-bot-opencv/websocket_server_mock.py
+++ tread-bot-opencv/websocket_server_mock.py
@@ -1,17 +1,49 @@
 import json
 from websocket_server import WebsocketServer
+from collections import deque
 
 # This file mainly for testing with websocket in own same computer before uploading to our website.
 
+# Create a waiting queue with a maximum size of 8
+waiting_queue = deque(maxlen=8)
+active_client = None
+
 def new_client(client, server):
-    print("Command client connected")
+    global waiting_queue, active_client
+
+    if not active_client:
+        active_client = client
+        server.send_message(client, json.dumps({"response": "You are now in control"}))
+    else:
+        waiting_queue.append(client)
+        server.send_message(client, json.dumps({"response": "You are in the waiting queue"}))
+        
+    print("New client connected")
 
 def client_left(client, server):
-    print("Command client disconnected")
+    global waiting_queue, active_client
+
+    if client == active_client:
+        active_client = None
+        if waiting_queue:
+            active_client = waiting_queue.popleft()
+            server.send_message(active_client, json.dumps({"response": "You are now in control"}))
+    else:
+        waiting_queue.remove(client)
+
+    print("Client disconnected")
 
 def message_received(client, server, message):
-    print("Message received: ", message)
-    data = json.loads(message)
+    global active_client
+
+    if client == active_client:
+        print("Message received: ", message)
+        data = json.loads(message)
+        response = {"response": "Message processed"}
+        server.send_message(client, json.dumps(response))
+    else:
+        response = {"response": "You are not in control"}
+        server.send_message(client, json.dumps(response))
 
 def start_websocket_server():
     server = WebsocketServer(port=10334, host='127.0.0.1')
