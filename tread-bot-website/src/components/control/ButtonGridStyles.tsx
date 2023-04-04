8bcde7b1e566b7826d162ed39656fc1a5076b22e
Olivia Chaves <tuj41026@temple.edu>
2023-04-04 18:27:22 -0400

    started making buttons resizable

--- tread-bot-website/src/components/control/ButtonGridStyles.tsx
+++ tread-bot-website/src/components/control/ButtonGridStyles.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { FONT_FAMILY } from '../../tools/Constants';
+import { FONT_FAMILY, COLORS } from '../../tools/Constants';
 
 export default class ButtonGridStyles {
 	static readonly ButtonGridContainer = styled.div`
@@ -18,16 +18,16 @@ export default class ButtonGridStyles {
 	static readonly DirectionButton = styled.button`
 		${FONT_FAMILY.BUTTON}
 
-		&:active {
-			box-shadow: 0px 0px 0px 0px;
-			top: 5px;
-			left: 5px;
-		}
-		
-		@media (min-width: 768px) {
-			padding: 0.25em 0.75em;
-		}
-	`
+        &:active {
+            box-shadow: 0px 0px 0px 0px;
+            top: 5px;
+            left: 5px;
+        }
+        
+        @media (min-width: 768px) {
+            padding: 0.25em 0.75em;
+        }
+    `
 
 	static readonly StopButton = styled.button`
 		${FONT_FAMILY.BUTTON}
@@ -35,16 +35,16 @@ export default class ButtonGridStyles {
     	background-size: 400% 400%;
     	animation: gradient 5s ease infinite;
 
-		&:active {
-			box-shadow: 0px 0px 0px 0px;
-			top: 5px;
-			left: 5px;
-		}
+        &:active {
+            box-shadow: 0px 0px 0px 0px;
+            top: 1vh;
+            left: 1vw;
+        }
 
-		@media (min-width: 768px) {
-			padding: 0.25em 0.75em;
-		}
-	`
+        @media (min-width: 768px) {
+            padding: 0.25vw 0.75vh;
+        }
+    `
 
 	static readonly Labels = styled.div`
 		font-family: "Open Sans", sans-serif;
@@ -57,19 +57,24 @@ export default class ButtonGridStyles {
 	`;
 
 	static readonly Modes = styled.button`
-		${FONT_FAMILY.BUTTON}
-		justify-self: center;
-		align-self: center;
-		text-align: center;
-
-		&:active {
-			box-shadow: 0px 0px 0px 0px;
-			top: 5px;
-			left: 5px;
-		}
-		
-		@media (min-width: 768px) {
-			padding: 0.25em 0.75em;
-		}
-	`
+		font-family: "Open Sans", sans-serif;
+		font-weight: bold;
+		font-size: 16px;
+		letter-spacing: 2px;
+		text-decoration: none;
+		text-transform: uppercase;
+		color: ${COLORS.TEXT};
+		margin-top: 30px;
+		margin-left: 90px;
+		height: 40px;
+		width: 150px;
+		border: 3px solid;
+		padding: 0.25em 0.5em;
+		box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;	
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		grid-area: auto;
+	`;
 }
+
