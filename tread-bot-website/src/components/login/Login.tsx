940f192129191f7c07b3780b3e0e6d84781183a0
Qian Peng <tug82109@temple.edu>
2023-04-04 23:48:45 -0400

    add listening "login" from web then user to waitQ

--- tread-bot-website/src/components/login/Login.tsx
+++ tread-bot-website/src/components/login/Login.tsx
@@ -1,98 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { io, Socket } from 'socket.io-client';
 import Styles from './LoginStyles';
 
 type LoginProps = {
-	loginSuccessful: Function
-}
+  loginSuccessful: Function;
+};
 
 const Login = (props: LoginProps): React.ReactElement => {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [error, setError] = useState('');
-	const [usernameFocused, setUsernameFocused] = useState(false);
-	const [passwordFocused, setPasswordFocused] = useState(false);
-	//for animation on login button when enter key is pressed
-	const [buttonFocused, setButtonFocused] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [usernameFocused, setUsernameFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
+  const [buttonFocused, setButtonFocused] = useState(false);
+	
+  // Initialize socket and connect to the robot_commands_ws server
+  const [socket, setSocket] = useState<Socket | null>(null);
+  useEffect(() => {
+    const newSocket = io('ws://100.14.184.218:10334'); //websocket address to robot
+    setSocket(newSocket);
 
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
 
-	const handleLogin = async () => {
-		setError('');
-		const endpoint = 'https://ryanhodge.net/login'
-		let response;
-		try {
-			if (window.location.hostname !== 'localhost') {
-				response = await fetch(endpoint, {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({ username, password })
-				});
-			}
+  const handleLogin = async () => {
+    setError('');
+    const endpoint = 'https://ryanhodge.net/login';
+    let response;
+    try {
+      if (window.location.hostname !== 'localhost') {
+        response = await fetch(endpoint, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password }),
+        });
+      }
 
-			if (window.location.hostname === 'localhost' || response?.ok) {
-				console.log('Login successful');
-				props.loginSuccessful(true)
-			} else {
-				const data = await response?.json();
-				setError(data.error);
-			}
-		} catch (error) {
-			console.error('Error:', error);
-		}
-		console.log('Login button clicked');
-	};
+	  if (window.location.hostname === 'localhost' || response?.ok) {
+        console.log('Login successful');
+        props.loginSuccessful(true);
 
-	//to handle enter keypress as login
-	const handleEnterKeyPress = (event: React.KeyboardEvent) => {
-		if (event.key === 'Enter') {
-			setButtonFocused(true);
-			handleLogin();
-			setTimeout(() => setButtonFocused(false), 100);
-		}
-	};
+        // Send login and username to the robot
+        if (socket) {
+          socket.emit('login', { username });
+        }
+      } else {
+        const data = await response?.json();
+        setError(data.error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    console.log('Login button clicked');
+  };
+  
+  const handleEnterKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      setButtonFocused(true);
+      handleLogin();
+      setTimeout(() => setButtonFocused(false), 100);
+    }
+  };
 
-
-	return (
-		<Styles.LoginContainer>
-			<Styles.Heading>Controls</Styles.Heading>
-			<Styles.InputContainer>
-				{!usernameFocused && !username && (
-					<label htmlFor="username">Username</label>
-				)}
-				<input
-					type="text"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-					style={{ paddingTop: '0.25rem' }}
-					onFocus={() => setUsernameFocused(true)}
-					onBlur={() => setUsernameFocused(false)}
-					onKeyDown={handleEnterKeyPress}
-				/>
-			</Styles.InputContainer>
-			<Styles.InputContainer>
-				{!passwordFocused && !password && (
-					<label htmlFor="password">Password</label>
-				)}
-				<input
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					style={{ paddingTop: '0.25rem' }}
-					onFocus={() => setPasswordFocused(true)}
-					onBlur={() => setPasswordFocused(false)}
-					onKeyDown={handleEnterKeyPress}
-				/>
-			</Styles.InputContainer>
-			<Styles.LoginButton
-				onClick={handleLogin}
-				isFocused={buttonFocused}
-			>
-				Login
-			</Styles.LoginButton>
-			<p style={{ color: error ? 'red' : 'blue', marginTop: '1cm' }}>
-				{error ? 'Username or Password is not correct, please try again.' : ''}
-			</p>
-		</Styles.LoginContainer>
-	);
+  return (
+    <Styles.LoginContainer>
+      <Styles.Heading>Controls</Styles.Heading>
+      <Styles.InputContainer>
+        {!usernameFocused && !username && (
+          <label htmlFor="username">Username</label>
+        )}
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          style={{ paddingTop: '0.25rem' }}
+          onFocus={() => setUsernameFocused(true)}
+          onBlur={() => setUsernameFocused(false)}
+          onKeyDown={handleEnterKeyPress}
+        />
+      </Styles.InputContainer>
+      <Styles.InputContainer>
+        {!passwordFocused && !password && (
+          <label htmlFor="password">Password</label>
+        )}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ paddingTop: '0.25rem' }}
+          onFocus={() => setPasswordFocused(true)}
+          onBlur={() => setPasswordFocused(false)}
+          onKeyDown={handleEnterKeyPress}
+        />
+      </Styles.InputContainer>
+	  <Styles.LoginButton
+        onClick={handleLogin}
+        isFocused={buttonFocused}
+      >
+        Login
+      </Styles.LoginButton>
+      <p style={{ color: error ? 'red' : 'blue', marginTop: '1cm' }}>
+        {error
+          ? 'Username or Password is not correct, please try again.'
+          : ''}
+      </p>
+    </Styles.LoginContainer>
+  );
 };
 
 export default Login;
--- tread-bot/new_adeept/robot_commands_ws.py
+++ tread-bot/new_adeept/robot_commands_ws.py
@@ -7,7 +7,7 @@ from move import arm_claw_control
 from queue import Queue
 
 class RobotCommandWS():
-	
+
 	PORT = 10334
 	HOST = '192.168.2.1'
 	HOST_PATH = 'ws://' + HOST + ':' + str(PORT)
@@ -34,7 +34,7 @@ class RobotCommandWS():
 					self.waiting_queue.put(ws)
 
 					# If active_queue is empty and waiting_queue has users, pop one user to active_queue
-					if self.active_queue.empty() and not self.waiting_queue.empty():
+					if self.active_queue.empty() and not self_waiting_queue.empty():
 						active_user = self.waiting_queue.get()
 						self.active_queue.put(active_user)
 
@@ -42,14 +42,21 @@ class RobotCommandWS():
 					while ws == self.active_queue.queue[0]:
 						msg = await ws.recv()
 						message_data = json.loads(msg)
-						print(message_data)
-						move.move(50, message_data['direction'], message_data['turn'], 0.5)
-						arm_command = message_data.get('arm_command', None)
-						if arm_command:
-							if arm_command in ['close', 'open', 'up', 'down']:
-								arm_claw_control(arm_command)
-							else:
-								pass
+
+						# Check for 'login' event
+						if message_data.get('event') == 'login':
+							username = message_data['username']
+							self.waiting_queue.put(username)
+							await ws.send(json.dumps({'event': 'waiting_queue_update', 'queue': list(self.waiting_queue.queue)}))
+						else:
+							print(message_data)
+							move.move(50, message_data['direction'], message_data['turn'], 0.5)
+							arm_command = message_data.get('arm_command', None)
+							if arm_command:
+								if arm_command in ['close', 'open', 'up', 'down']:
+									arm_claw_control(arm_command)
+								else:
+									pass
 
 					# If websocket is closed, remove it from active_queue and waiting_queue
 					self.active_queue.get(ws)
