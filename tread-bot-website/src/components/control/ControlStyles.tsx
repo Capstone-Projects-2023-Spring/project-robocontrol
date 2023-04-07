611b8e1c8f91115dc9593522f1df27d7b67c6216
ryanhodge240@gmail.com <ryanhodge240@gmail.com>
2023-04-07 18:51:35 -0400

    Fixed tabs

--- tread-bot-website/src/components/control/ControlStyles.tsx
+++ tread-bot-website/src/components/control/ControlStyles.tsx
@@ -12,32 +12,32 @@ export default class ControlStyles {
 		width: 100%;
 	`;
 
-    static readonly VideoFeedContainer = styled.div`
-        display: grid;
-        grid-template-columns: repeat(2);
+	static readonly VideoFeedContainer = styled.div`
+		display: grid;
+		grid-template-columns: repeat(2);
 		grid-gap: 50px;
-        justify-content: center;
-        margin-top: 100px;
-        align-items: center;
-        width: 95%;
+		justify-content: center;
+		margin-top: 100px;
+		align-items: center;
+		width: 95%;
 		padding: 5vw;
 
-        img:first-child {
-            grid-column: 1;
-            color: ${COLORS.TEXT};
-            border: 3px solid;
-            padding: 0.25em;
-            box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3); 
-        }
+		img:first-child {
+			grid-column: 1;
+			color: ${COLORS.TEXT};
+			border: 3px solid;
+			padding: 0.25em;
+			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3); 
+		}
 
-        img:last-child {
-            grid-column: 2;
-            color: ${COLORS.TEXT};
-            border: 3px solid;
-            padding: 0.25em;
-            box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3);
-        }
-    `
+		img:last-child {
+			grid-column: 2;
+			color: ${COLORS.TEXT};
+			border: 3px solid;
+			padding: 0.25em;
+			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3);
+		}
+	`
 
 	static readonly FlexContainer = styled.div`
 		display: flex;
