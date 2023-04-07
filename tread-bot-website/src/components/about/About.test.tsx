2a5ad0b551446ed8b481feca90ed55a97d2a9e93
antKeyes <keyes.anthony@gmail.com>
2023-04-07 01:56:10 -0400

    Test for About component

--- tread-bot-website/src/components/about/About.test.tsx
+++ tread-bot-website/src/components/about/About.test.tsx
@@ -2,3 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import About from './About';
+
+
+// test to check if the About component renders without any issues
+
+describe('About component', () => {
+    test('renders About component', () => {
+      render(<About />);
+      const aboutElement = screen.getByTestId('about-container');
+      expect(aboutElement).toBeInTheDocument();
+    });
+  });
+  
