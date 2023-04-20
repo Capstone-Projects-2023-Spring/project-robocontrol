0ad2063b6eb02c77de68a52cd3af023b1fc0bcc7
Olivia Chaves <tuj41026@temple.edu>
2023-04-20 15:08:58 -0400

    Changing formatting

--- tread-bot-website/src/components/control/ButtonGridStyles.tsx
+++ tread-bot-website/src/components/control/ButtonGridStyles.tsx
@@ -8,34 +8,34 @@ export default class ButtonGridStyles {
 		display: grid;
 		grid-template-columns: repeat(9, minmax(0, 1fr));
 		grid-template-columns: 
-            repeat(3, calc(var(--vh) * 0.1))
-            calc(var(--vh) * 0.016) // Gap
-            repeat(3, calc(var(--vh) * 0.1))
-            calc(var(--vh) * 0.016) // Gap
-            calc(var(--vh) * 0.1);
+			repeat(3, calc(var(--vh) * 0.1))
+			calc(var(--vh) * 0.016) // Gap
+			repeat(3, calc(var(--vh) * 0.1))
+			calc(var(--vh) * 0.016) // Gap
+			calc(var(--vh) * 0.1);
 		grid-template-rows: repeat(6, calc(var(--vh) * 0.1));
 		gap: calc(var(--vh) * 0.016);
 		justify-content: center;
 		align-content: center;
 		align-self: start;
-        padding-left: calc(var(--vh) * 0.01);
-        padding-right: calc(var(--vh) * 0.01);
+		padding-left: calc(var(--vh) * 0.01);
+		padding-right: calc(var(--vh) * 0.01);
 	`;
 
 	static readonly DirectionButton = styled.button`
 		${FONT_FAMILY.BUTTON}
-        &:active { box-shadow: 0px 0px 0px 0px; }
-        @media (min-width: 768px) { padding: 0.25em 0.75em; }
-    `
+		&:active { box-shadow: 0px 0px 0px 0px; }
+		@media (min-width: 768px) { padding: 0.25em 0.75em; }
+	`
 
 	static readonly StopButton = styled.button`
 		${FONT_FAMILY.BUTTON}
 		background: linear-gradient(-45deg, #F894A4, #F9D1B7);
-    	background-size: 400% 400%;
-    	animation: gradient 5s ease infinite;
-        &:active { box-shadow: 0px 0px 0px 0px; }
-        @media (min-width: 768px) { padding: 0.25vw 0.75vh; }
-    `
+		background-size: 400% 400%;
+		animation: gradient 5s ease infinite;
+		&:active { box-shadow: 0px 0px 0px 0px; }
+		@media (min-width: 768px) { padding: 0.25vw 0.75vh; }
+	`
 
 	static readonly Labels = styled.div`
 		font-family: "Open Sans", sans-serif;
--- tread-bot-website/src/components/control/ControlStyles.tsx
+++ tread-bot-website/src/components/control/ControlStyles.tsx
@@ -2,26 +2,26 @@ import styled from 'styled-components'
 import { COLORS } from '../../tools/Constants' 	
 
 export default class ControlStyles {
-    static readonly ControlContainer = styled.div`
-        height: 100%;
+	static readonly ControlContainer = styled.div`
+		height: 100%;
 		display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: calc(var(--vh) * 0.4) calc(var(--vh) * 0.7) calc(var(--vh) * 0.5);
-        justify-items: center;
-        align-items: center;
+		grid-template-columns: repeat(2, 1fr);
+		grid-template-rows: calc(var(--vh) * 0.4) calc(var(--vh) * 0.7) calc(var(--vh) * 0.5);
+		justify-items: center;
+		align-items: center;
 
-        & > img {
-            width: 90%;
-            max-height: 90%;
-            color: ${COLORS.TEXT};
-            border: 3px solid;
-            padding: 0.25em;
-            box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3); 
-        }
+		& > img {
+			width: 90%;
+			max-height: 90%;
+			color: ${COLORS.TEXT};
+			border: 3px solid;
+			padding: 0.25em;
+			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3); 
+		}
 	`
 
-    static readonly ButtonContainer = styled.div`
-        height: 100%;
-        grid-area: 2 / 1 / 2 / 3;
-    `
+	static readonly ButtonContainer = styled.div`
+		height: 100%;
+		grid-area: 2 / 1 / 2 / 3;
+	`
 }
