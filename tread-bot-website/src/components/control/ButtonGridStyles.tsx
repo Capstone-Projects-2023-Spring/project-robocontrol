d28b7e5cab1682332a3b28a488e764c155487e16
Olivia Chaves <tuj41026@temple.edu>
2023-04-20 15:09:56 -0400

    fixed button grid styles

--- tread-bot-website/src/components/control/ButtonGridStyles.tsx
+++ tread-bot-website/src/components/control/ButtonGridStyles.tsx
@@ -1,124 +1,58 @@
 import styled from 'styled-components'
-import { COLORS } from '../../tools/Constants'  
+import { FONT_FAMILY } from '../../tools/Constants';
 
 export default class ButtonGridStyles {
+	static readonly ButtonGridContainer = styled.div`
+		height: 100%;
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(9, minmax(0, 1fr));
+		grid-template-columns: 
+			repeat(3, calc(var(--vh) * 0.1))
+			calc(var(--vh) * 0.016) // Gap
+			repeat(3, calc(var(--vh) * 0.1))
+			calc(var(--vh) * 0.016) // Gap
+			calc(var(--vh) * 0.1);
+		grid-template-rows: repeat(6, calc(var(--vh) * 0.1));
+		gap: calc(var(--vh) * 0.016);
+		justify-content: center;
+		align-content: center;
+		align-self: start;
+		padding-left: calc(var(--vh) * 0.01);
+		padding-right: calc(var(--vh) * 0.01);
+	`;
 
-    static readonly ButtonGridContainer = styled.div`
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(10vw, 1fr));
-        grid-template-rows: repeat(auto-fit, minmax(12vh, 2fr));
-        gap: 15px;
-        justify-content: center;
-        align-content: center;
-        width: 95%;
-    `;
+	static readonly DirectionButton = styled.button`
+		${FONT_FAMILY.BUTTON}
+		&:active { box-shadow: 0px 0px 0px 0px; }
+		@media (min-width: 768px) { padding: 0.25em 0.75em; }
+	`
 
-    static readonly DirectionButton = styled.button`
-        font-family: "Open Sans", sans-serif;
-        font-weight: bold;
-        font-size: 2vw;
-        letter-spacing: .2vw;
-        text-decoration: none;
-        text-transform: uppercase;
-        color: ${COLORS.TEXT};
-        cursor: pointer;
-        border: 3px solid;
-        padding: 1vw 1vw;
-        box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 15px 15px 13px rgba(0, 0, 0, 0.3); 
-        position: relative;
-        user-select: none;
-        -webkit-user-select: none;
-        touch-action: manipulation;
-        white-space: pre;
+	static readonly StopButton = styled.button`
+		${FONT_FAMILY.BUTTON}
+		background: linear-gradient(-45deg, #F894A4, #F9D1B7);
+		background-size: 400% 400%;
+		animation: gradient 5s ease infinite;
+		&:active { box-shadow: 0px 0px 0px 0px; }
+		@media (min-width: 768px) { padding: 0.25vw 0.75vh; }
+	`
 
+	static readonly Labels = styled.div`
+		font-family: "Open Sans", sans-serif;
+		font-weight: bold;
+		font-size: calc(var(--vh) * 0.016);
+		letter-spacing: 2px;
+		padding: 0.25em 0.5em;
+		display: flex;
+		align-items: center;
+	`;
 
-        &:active {
-            box-shadow: 0px 0px 0px 0px;
-            top: 5px;
-            left: 5px;
-        }
-        
-        @media (min-width: 768px) {
-            padding: 0.25em 0.75em;
-        }
-    `
-
-    static readonly StopButton = styled.button`
-        font-family: "Open Sans", sans-serif;
-        font-size: 3vw;
-        font-weight: bold;
-        letter-spacing: .2vw;
-        text-decoration: none;
-        text-transform: uppercase;
-        text-align: center;
-        color: ${COLORS.TEXT};
-        cursor: pointer;
-        border: 3px solid;
-        padding: .5vw .5vw;
-        box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 15px 15px 13px rgba(0, 0, 0, 0.3); 
-        position: relative;
-        user-select: none;
-        -webkit-user-select: none;
-        touch-action: manipulation;
-
-        background: linear-gradient(-45deg, #F894A4, #F9D1B7);
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        animation: gradient 5s ease infinite;
-
-        &:active {
-            box-shadow: 0px 0px 0px 0px;
-            top: 1vh;
-            left: 1vw;
-        }
-
-        @media (min-width: 768px) {
-            padding: 0.25vw 0.75vh;
-        }
-    `
-
-
-
-    // Add a new styled component called Label
-    static readonly Labels = styled.div`
-        font-family: "Open Sans", sans-serif;
-        font-weight: bold;
-        font-size: 3vw;
-        letter-spacing: .2vw;
-        text-decoration: none;
-        text-transform: uppercase;
-        color: ${COLORS.TEXT};
-        margin-top: 30px;
-        margin-left: 90px;
-        height: 8vw; //changed this
-        width: 16vw;
-        border: 3px solid;
-        padding: 0.25em 0.5em;
-        box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;    
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        grid-area: auto;
-        min-width: min-content; 
-    `;
-
-    static readonly Modes = styled.button`
-        font-family: "Open Sans", sans-serif;
-        font-weight: bold;
-        font-size: 2vw;
-        letter-spacing: .2vw;
-        text-decoration: none;
-        text-transform: uppercase;
-        color: ${COLORS.TEXT};
-        border: 3px solid;
-        //got rid of margins and borders because they wren't playing nice with flexible sizing
-        padding: 0.25em 0.5em;
-        box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;    
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        grid-area: auto;
-        min-width: min-content; //currently won't let button go smaller than word, is this how we want to go?
-        `
+	static readonly Modes = styled.button`
+		${FONT_FAMILY.BUTTON}
+		justify-self: center;
+		align-self: center;
+		text-align: center;
+		&:active { box-shadow: 0px 0px 0px 0px; }
+		@media (min-width: 768px) { padding: 0.25em 0.75em; }
+	`;
 }
