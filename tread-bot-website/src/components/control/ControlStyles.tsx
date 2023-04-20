39cee5f302839b672b03052e82721b6db98ed1e7
Olivia Chaves <tuj41026@temple.edu>
2023-04-20 15:12:15 -0400

    Changing sizing in the control styles

--- tread-bot-website/src/components/control/ControlStyles.tsx
+++ tread-bot-website/src/components/control/ControlStyles.tsx
@@ -3,44 +3,25 @@ import { COLORS } from '../../tools/Constants'
 
 export default class ControlStyles {
 	static readonly ControlContainer = styled.div`
+		height: 100%;
 		display: grid;
-		grid-template-columns: repeat(7, 10vw);
-		grid-template-rows: repeat(4, 12vh);
-		gap: 1.5vw;
-		justify-content: center;
-		align-content: center;
-		width: 100%;
-	`;
-
-	static readonly VideoFeedContainer = styled.div`
-		display: flex;
-		justify-content: center;
+		grid-template-columns: repeat(2, 1fr);
+		grid-template-rows: calc(var(--vh) * 0.4) calc(var(--vh) * 0.7) calc(var(--vh) * 0.5);
+		justify-items: center;
 		align-items: center;
-		width: 95%;
-		padding: 5vw;
 
-		img:first-child {
-			grid-column: 1;
+		& > img {
+			width: 90%;
+			max-height: 90%;
 			color: ${COLORS.TEXT};
 			border: 3px solid;
 			padding: 0.25em;
-			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3);	
-		}
-
-		img:last-child {
-			grid-column: 2;
-			color: ${COLORS.TEXT};
-			border: 3px solid;
-			padding: 0.25em;
-			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3);	
+			box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px, 6px 6px 0px 0px, 7px 7px 0px 0px, 20px 20px 13px rgba(0, 0, 0, 0.3); 
 		}
 	`
 
-
-	static readonly FlexContainer = styled.div`
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		width: 100%;
+	static readonly ButtonContainer = styled.div`
+		height: 100%;
+		grid-area: 2 / 1 / 2 / 3;
 	`
 }
