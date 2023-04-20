83771d8f9eb320bf97c3d92645e30164a8ab9ecc
Olivia Chaves <tuj41026@temple.edu>
2023-04-20 14:35:20 -0400

    new commit

--- tread-bot-website/src/components/control/ButtonGridStyles.tsx
+++ tread-bot-website/src/components/control/ButtonGridStyles.tsx
@@ -57,24 +57,19 @@ export default class ButtonGridStyles {
 	`;
 
 	static readonly Modes = styled.button`
-		font-family: "Open Sans", sans-serif;
-		font-weight: bold;
-		font-size: 16px;
-		letter-spacing: 2px;
-		text-decoration: none;
-		text-transform: uppercase;
-		color: ${COLORS.TEXT};
-		margin-top: 30px;
-		margin-left: 90px;
-		height: 40px;
-		width: 150px;
-		border: 3px solid;
-		padding: 0.25em 0.5em;
-		box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;	
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		grid-area: auto;
+		${FONT_FAMILY.BUTTON}
+		justify-self: center;
+		align-self: center;
+		text-align: center;
+		&:active {
+			box-shadow: 0px 0px 0px 0px;
+			top: 5px;
+			left: 5px;
+		}
+		
+		@media (min-width: 768px) {
+			padding: 0.25em 0.75em;
+		}
 	`;
 }
 
