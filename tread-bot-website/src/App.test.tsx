1345e130071cc9714b97f3e06a8fc3f98d74e185
Ryan Hodge <ryanhodge240@gmail.com>
2023-04-22 02:12:05 -0400

    About page testing is working

--- tread-bot-website/src/App.test.tsx
+++ tread-bot-website/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
 // Crazy voodoo magic
@@ -17,15 +17,107 @@ beforeAll(() => Object.defineProperty(window, 'matchMedia', {
 	})),
 }));
 
+beforeEach(() => {
+	global.fetch = jest.fn(() =>
+		Promise.resolve({
+			text: () => Promise.resolve({ text: 'hello' }),
+		}),
+	) as jest.Mock;
+	global.scrollTo = jest.fn()
+})
+
 test('Render whole app', () => {
 	render(<App />);
 	const linkElement = screen.getByText('CONTROL');
-	expect(linkElement.textContent).toEqual('CONTROL');
+	expect(linkElement.textContent).toEqual('CONTROL'); // TODO: Change test?
 });
 
-test('Open offcanvas menu', () => {
-	render(<App />);
-	fireEvent.click( screen.getByTestId('hamburger') )
-	const linkText = screen.getByText('HOME');
-	expect(linkText).toBeDefined()
+describe('Navigation', () => {
+	test('Open control page', () => {
+		render(<App />);
+		fireEvent.click(screen.getByTestId('control-button'))
+		const linkText = screen.getByTestId('Control');
+		expect(linkText).toBeDefined()
+	});
+
+	test('Open about page', async () => {
+		render(<App />);
+		fireEvent.click(screen.getByTestId('hamburger'))
+		fireEvent.click(screen.getByTestId('banner-home'))
+		fireEvent.click(screen.getByTestId('about-button'))
+		await waitFor(async () => {
+			let linkText = await screen.findByTestId('About')
+			expect(linkText).toBeDefined()
+		})
+	});
+
+	// describe('Offcanvas menu', () => {
+	// 	test('Open offcanvas menu', () => {
+	// 		render(<App />);
+	// 		fireEvent.click(screen.getByTestId('hamburger'))
+	// 		const linkText = screen.getByText('HOME');
+	// 		expect(linkText).toBeDefined()
+	// 	});
+
+	// 	test('Navigate to controls page from offcanvas', () => {
+	// 		render(<App />);
+	// 		fireEvent.click(screen.getByTestId('hamburger'))
+	// 		fireEvent.click( screen.getByTestId('banner-controls'))
+	// 		const linkText = screen.getByText('Controls');
+	// 		expect(linkText).toBeDefined()
+	// 	});
+
+	// 	test('Navigate to about page from offcanvas', () => {
+	// 		render(<App />);
+	// 		fireEvent.click(screen.getByTestId('hamburger'))
+	// 		fireEvent.click(screen.getByTestId('banner-about'))
+	// 		const linkText = screen.getByText('HOME');
+	// 		expect(linkText).toBeDefined()
+	// 	});
+
+	// 	test('Navigate back to home page from offcanvas', () => {
+	// 		render(<App />);
+	// 		fireEvent.click(screen.getByTestId('hamburger'))
+	// 		fireEvent.click(screen.getByTestId('banner-about'))
+	// 		fireEvent.click(screen.getByTestId('hamburger'))
+	// 		fireEvent.click(screen.getByTestId('banner-home'))
+	// 		const linkText = screen.getByText('HOME');
+	// 		expect(linkText).toBeDefined()
+	// 	});
+	// })
 })
+
+// describe('Control', () => {
+	
+// 	test('Open control page', () => {
+// 		render(<App />);
+// 		fireEvent.click(screen.getByTestId('control-button'))
+// 		const linkText = screen.getByText('Controls');
+// 		expect(linkText).toBeDefined()
+// 	});
+
+// 	test('Login to controls', () => {
+// 		render(<App />);
+// 		fireEvent.click(screen.getByTestId('control-button'))
+// 		const linkText = screen.getByText('Controls');
+// 		expect(linkText).toBeDefined()
+// 	});
+// })
+
+// describe('About', () => {
+// 	test('Open offcanvas menu', () => {
+// 		render(<App />);
+// 		fireEvent.click(screen.getByTestId('hamburger'))
+// 		const linkText = screen.getByText('HOME');
+// 		expect(linkText).toBeDefined()
+// 	});
+// })
+
+// describe('Other', () => {
+// 	test('Open offcanvas menu', () => {
+// 		render(<App />);
+// 		fireEvent.click(screen.getByTestId('hamburger'))
+// 		const linkText = screen.getByText('HOME');
+// 		expect(linkText).toBeDefined()
+// 	});
+// })
